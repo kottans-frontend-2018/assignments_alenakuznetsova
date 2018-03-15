b6f54f3ef8c2cda38dcae406076bfd60c62ab4f6
alena kuznetsova <alena.klevtsova@gmail.com>
2018-03-15 14:39:45 +0200

    add some changes for register page

--- task_12/src/components/RegistrationForm.js
+++ task_12/src/components/RegistrationForm.js
@@ -7,8 +7,10 @@ class RegistrationForm {
             `<form class="registration-form">
                 <span class="registration-form__title">registration</span>
                 <input type="text" placeholder="Name">
+                <input type="email" placeholder="email">
                 <input type="text" placeholder="login">
-                <input type="text" placeholder="password">
+                <input type="password" placeholder="password">
+                <input type="password" placeholder="repeat password">
                 <button class="btn-registration">registration</button>
                 <p class="message">Already registered?<a href="#login">&nbsp;Login</a></p>
             </form>`;
