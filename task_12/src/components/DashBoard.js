86a3ff5217cb2e24b52eebe50d5d6892d110e862
alena kuznetsova <alena.klevtsova@gmail.com>
2018-03-26 14:35:43 +0300

    add ssh connetction

--- task_12/src/components/DashBoard.js
+++ task_12/src/components/DashBoard.js
@@ -63,6 +63,19 @@ class DashBoard {
                 </div>
             </div>
         </div>
+         <div class="pizza-container">
+            <div class="pizza-img">
+                <img class="img" src="${url}">
+            </div>
+            <div class="pizza-order-info">
+                <div class="pizza-order-info__left">
+                    <span>xx</span><span>xx</span>
+                </div>
+                <div class="pizza-order-info__right">
+                    <span>xx</span><span>xx</span>
+                </div>
+            </div>
+        </div>
         <button id='btn-add' class='btn-add' type='button'>+ Add new pizza</button>`;
     }
 
